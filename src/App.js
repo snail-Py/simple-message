2e093cb90eeb73db34b80af975a617c9b3b53c5a
snail-Py <cs.legoff@gmail.com>
2022-04-05 19:50:54 +0800

    Created simple message app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  // State
+  
+  let [message, setMessage] = useState(
+    "JavaScript is the best language in the world!");
+  
+  // Actions
+  
+  function handleClick() {
+    setMessage("Some new message");
+
+  }
+
+  // UI
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{message}</h1>
+      <button onClick={handleClick}>Update the message</button>
     </div>
   );
 }
